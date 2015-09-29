a3802a37dd858528aa243c7f4bddb838acb09061
Bryant Syme <prefu@hotmail.com>
2015-09-28 22:05:13 -0700

    stub out more shuffler tests

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
 'use strict';
 
 describe('shuffler', function () {
-  it('shuffles', function () {});
+  it('returns cards in a different order', function () {
+
+  });
+  it('returns the same number as given', function () {
+
+  });
+  it('includes all the cards', function () {
+    
+  });
 });
